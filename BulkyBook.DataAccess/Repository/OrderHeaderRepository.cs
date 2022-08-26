cf2f4666f17ca6e2b2e3c0ff698b7dd4de525730
zuka meliqishvili <zuka.meliqishvili@gmail.com>
2022-08-26 19:33:26 +0400

    added orderHeader and orderdetails tables to db and created orderheader and orderdetails repositories. created summary action method

---
+++ BulkyBook.DataAccess/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,37 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private ApplicationDbContext db;
+        public OrderHeaderRepository(ApplicationDbContext db):base(db)
+        {
+            this.db = db;
+        }
+
+        public void Update(OrderHeader obj)
+        {
+            db.OrderHeaders.Update(obj);
+        }
+        public void UpdateStatus(int id, string orderStatus, string? paymentStatus = null)
+        {
+            var orderFromDb = db.OrderHeaders.FirstOrDefault(u => u.Id == id);
+            if(orderFromDb!=null)
+            {
+                orderFromDb.OrderStatus = orderStatus;
+                if(paymentStatus!=null)
+                {
+                    orderFromDb.PaymentStatus = paymentStatus;
+                }
+            }
+        }
+
+    }
+}
